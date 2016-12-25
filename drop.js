7cb9974ab1b1f4ec6826f3106730fd8c55d30382
Guillaume Blackburn <guillaumeblackburn@gmail.com>
2016-12-25 14:42:58 -0500

    V0
    Official version

--- drop.js
+++ drop.js
@@ -1,15 +1,9 @@
-// Daniel Shiffman
-// http://codingrainbow.com
-// http://patreon.com/codingrainbow
-// Code for: https://youtu.be/KkyIDI6rQJI
-
-
 function Drop() {
   this.x = random(width);
   this.y = random(-500, -50);
   this.z = random(0, 20);
-  this.len = map(this.z, 0, 20, 10, 20);
-  this.yspeed = map(this.z, 0, 20, 1, 5);
+  //this.len = map(this.z, 0, 20, 10, 20);
+  this.yspeed = map(this.z, 0, 20, 1, 2);
 
   this.fall = function() {
     this.y = this.y + this.yspeed;
@@ -17,14 +11,15 @@ function Drop() {
     this.yspeed = this.yspeed + grav;
 
     if (this.y > height) {
-      this.y = random(-200, -100);
-      this.yspeed = map(this.z, 0, 20, 2, 8);
+      this.y = random(-500, -50);
+      this.yspeed = map(this.z, 0, 20, 1, 2);
     }
   }
 
   this.show = function() {
-    var thick = map(this.z, 0, 20, 1, 3);
+    var thick = map(this.z, 0, 20, 0.5, 3);
     strokeWeight(thick);
+    fill(255,255,255)
     stroke(255, 255, 255);
     //line(this.x, this.y, this.x, this.y+this.len);
     ellipse(this.x, this.y, 3,3);
